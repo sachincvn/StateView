8a2aaad2e39b588206b1510e3068578cbbd87304
Sachin Chavan <sachin.c@gharoffice.com>
2022-12-29 14:08:11 +0530

    Add project files.

---
+++ StateView/StateView/Controls/SkeletonView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StateView.Controls
+{
+    public class SkeletonView : BoxView
+    {
+        public SkeletonView()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(1.5), () =>
+            {
+                this.FadeTo(0.5, 750, Easing.CubicInOut).ContinueWith((x) =>
+                {
+                    this.FadeTo(1, 750, Easing.CubicInOut);
+                });
+
+                return true;
+            });
+        }
+    }
+}
